e52ff95b79e7fe1cec45009b31cf451278acaa4a
ductuantruong <ductuanpy99@gmail.com>
2022-06-10 19:36:58 +0800

    fix bug in preprocessing data

--- LRE17/prepare_lre17_data.py
+++ LRE17/prepare_lre17_data.py
@@ -8,14 +8,17 @@ my_parser = argparse.ArgumentParser(description='Path to the TIMIT dataset folde
 my_parser.add_argument('--path',
                        metavar='path',
                        type=str,
+                       default='data/wav',
                        help='the path to dataset folder')
 my_parser.add_argument('--train_folder_name',
                        metavar='train_folder_name',
                        type=str,
+                       default='lre-17-train',
                        help='the name of train data folder')
 my_parser.add_argument('--val_folder_name',
                        metavar='val_folder_name',
                        type=str,
+                       default='lre-17-eval',
                        help='the name of validate data folder')
 args = my_parser.parse_args()
 
@@ -29,8 +32,8 @@ val_data_path = os.path.join(original_data_dir, val_folder_name)
 
 list_lang_folder = []
 for folder in os.listdir(train_data_path):
-    if os.path.isdir(folder):
-        list_lang_folder.append(os.path.join(train_data_path, folder))
+    if os.path.isdir(os.path.join(train_data_path, folder)):
+        list_lang_folder.append(folder)
 
 dict_data_type = {
                     'TRN': train_data_path, 
@@ -44,10 +47,9 @@ for data_type in dict_data_type.keys():
     print('Processing {} dataset'.format(data_type))
     data_path = dict_data_type[data_type]
     for lang_name in list_lang_folder:
-        data_path = os.path.join(data_path, lang_name)
         group_lang = lang_name.split('_')[0]
         lang = lang_name.split('_')[1]
-        for wav_file in os.listdir(data_path):
+        for wav_file in os.listdir(os.path.join(data_path, lang_name)):
             data_record = {
                 'utt_id': wav_file[:-4],
                 'data_type': data_type,
@@ -57,5 +59,5 @@ for data_type in dict_data_type.keys():
             }
             list_data_record.append(data_record)
 data_df = pd.concat([data_df, pd.DataFrame.from_records(list_data_record)], ignore_index=True)
-data_df.to_csv(os.path.join(original_data_dir, 'data_info_age.csv'), index=False)
+data_df.to_csv(os.path.join(original_data_dir, 'data_info.csv'), index=False)
 print('Data saved at ', original_data_dir)
