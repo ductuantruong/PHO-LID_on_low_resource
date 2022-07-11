add model when load from wav & fix attention mask

Model/models.py
@@ -88,9 +88,14 @@ class CNN_Trans_LID(nn.Module):
 class PHOLID(nn.Module):
     def __init__(self, input_dim, feat_dim,
                  d_k, d_v, d_ff, n_heads=8,
-                 dropout=0.1, n_lang=14, max_seq_len=10000, is_train=False):
+                 dropout=0.1, n_lang=14, max_seq_len=10000, ssl_model=None):
         super(PHOLID, self).__init__()
-        self.is_train = is_train
+        self.ssl_model = ssl_model
+        if ssl_model:
+            self.upstream = torch.hub.load('s3prl/s3prl', ssl_model)
+            for param in self.upstream.parameters():
+                param.requires_grad = False
+ 
         self.input_dim = input_dim
 
         self.d_model = feat_dim * n_heads
@@ -135,23 +140,25 @@ class PHOLID(nn.Module):
         std = torch.sqrt(variance.transpose(0, 1))
         return torch.cat((correct_mean, std), dim=1)
 
-    def forward(self, utt, seq_len, eps=1e-5):
+    def forward(self, utt, seq_len, is_train=False, eps=1e-5):
+        if self.upstream:
+            utt = [torch.narrow(utt, 0, 0, seq_len[i]) for (i, utt) in enumerate(utt.squeeze(1))]
+            batch_size = len(utt)
+            utt = self.upstream(x)['hidden_state_16']
+            utt = utt.view(batch_size, -1, 20, 1024)
         batch_size = utt.size(0)
         T_len = utt.size(1)
-        # print('utt', utt.shape)
         x = utt.view(batch_size * T_len, -1, self.input_dim).transpose(-1, -2)
-        # print('shared_TDNN', x.shape)
         x = self.shared_TDNN(x)
         pho_x = x.transpose(-1, -2)
-        # print('after shared_TDNN', pho_x.shape)
         pho_out = self.phoneme_proj(pho_x)
 
-        # if self.is_train:
-            # shape = x.size()
-            # noise = torch.Tensor(shape)
-            # noise = noise.type_as(x)
-            # torch.randn(shape, out=noise)
-            # x += noise * eps
+        if is_train:
+            shape = x.size()
+            noise = torch.Tensor(shape)
+            noise = noise.type_as(x)
+            torch.randn(shape, out=noise)
+            x += noise * eps
 
         seg_stats = torch.cat((x.mean(dim=2), x.std(dim=2, unbiased=False)), dim=1)
         embedding = self.fc_xv(seg_stats)
@@ -162,7 +169,7 @@ class PHOLID(nn.Module):
         output = output.unsqueeze(1).repeat(1, self.n_heads, 1, 1)
         output = output.transpose(1, 2).contiguous().view(batch_size, -1, self.d_model)
 
-        if self.is_train:
+        if is_train:
             atten_mask = get_atten_mask(seq_len, utt.size(0))
             atten_mask = atten_mask.to(self.device)
             mean_mask_, weight_mean = mean_mask(seq_len, len(seq_len), dim=self.feat_dim * self.n_heads)
@@ -173,7 +180,7 @@ class PHOLID(nn.Module):
         output, _ = self.attention_block1(output, atten_mask)
         output, _ = self.attention_block2(output, atten_mask)
 
-        if self.is_train:
+        if is_train:
             stats = self.mean_std_pooling(output, batch_size, seq_len, mean_mask_, weight_mean,
                                           std_mask_, weight_unbaised)
         else: